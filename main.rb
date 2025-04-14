38cdb41b618f89f9f711911c3b71df90ecbeb2af
Andrew Roth <a.roth.80@hotmail.com>
2025-04-14 16:38:10 -0700

    enumeration with candidates

--- main.rb
+++ main.rb
@@ -1,9 +1,18 @@
-# This is the main entrypoint into the program
-# It requires the other files/gems that it needs
+# This is the main entry point into the program.
+# It requires the other files/gems that it needs.
+
 require 'pp'
 require './candidates'
 require './filters'
 
-## Your test code can go here
+# Test code can go here.
 
 pp @candidates
+puts "Experienced Candidates:"
+pp @candidates.select { |candidate| experienced?(candidate) }
+
+puts "Qualified Candidates:"
+pp qualified_candidates(@candidates)
+
+puts "Ordered by Qualifications:"
+pp ordered_by_qualifications(@candidates)
